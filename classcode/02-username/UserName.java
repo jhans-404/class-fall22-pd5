d07fadb6a607e401615b46995fb9fd2b339a44f9
Joshua Hans <jhans@schools.nyc.gov>
2022-10-21 12:19:07 -0400

    makes a random password

--- classcode/02-username/UserName.java
+++ classcode/02-username/UserName.java
@@ -5,6 +5,7 @@ public class UserName {
     // create Scanner object
     Scanner s = new Scanner(System.in);
 
+    /*
     // get String input from user
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
@@ -13,8 +14,13 @@ public class UserName {
 
     // test output
     System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
+    */
 
-    generatePassword(8);
+    System.out.print("How long of a password? ");
+    int len = s.nextInt();
+
+    System.out.println(generatePassword(len));
+
     s.close();
   } // end main method
 
@@ -36,36 +42,42 @@ public class UserName {
    */
   public static String generatePassword(int length) {
     String password = "";
+    int min, max;
 
     for(int i = 0; i < length; i++) {
       // generate a random number
       // 65 - 90 capital letter
       // 97 - 122 lower case
       // 33 - 47 special symbols
-      int rand = (int)(Math.random()*(90-65+1) + 65);
+      int choice = (int)(Math.random()*(3) + 1);
+
+      if (choice == 1) {
+        min = 65;
+        max = 90;
+      }
+
+      else if (choice == 2) {
+        min = 97;
+        max = 122;
+      }
+
+      else {
+        min = 33;
+        max = 47;
+      }
+
+      int rand = (int)(Math.random()*(max-min+1) + min);
 
       // convert the int ---> character c =(char)i;
       char c = (char)rand;
-      System.out.println(rand + " " + c);
-      
-      // add the char to the password String
+      // System.out.println(rand + " " + c);
 
+      // add the char to the password String
+      password = password + c; // password += c;
 
     } // end for loop
 
-
-
     return password;
   } // end generatePassword method
-
-
-
-
-
-
-
-
-
-
 
 } // end class
