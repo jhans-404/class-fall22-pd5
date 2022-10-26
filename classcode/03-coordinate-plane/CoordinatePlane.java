6cff08d8c74792f58cd13c774bc80c1bd5efe7b1
Joshua Hans <jhans@schools.nyc.gov>
2022-10-26 14:45:48 -0400

    can check input

--- classcode/03-coordinate-plane/CoordinatePlane.java
+++ classcode/03-coordinate-plane/CoordinatePlane.java
@@ -7,21 +7,26 @@ public class CoordinatePlane {
 
     System.out.print("Please enter a point in the form (x, y): ");
     // write a statement to store the input
+    point1 = s.nextLine();
 
     // check if the user input the point correctly
     // if yes, continue code
     // if no, get the input again
+    System.out.println(checkInput(point1));
 
     System.out.print("Please enter a second point: ");
     // write a statement to store the input
+    point2 = s.nextLine();
 
     // check if the user input the point correctly
     // if yes, continue code
     // if no, get the input again
-
+    System.out.println(checkInput(point2));
 
     // test your methods below:
-
+    int x1 = getX(point1);
+    int x2 = getX(point2);
+    System.out.println(x1 + " " + x2);
 
   } // end main method
 
@@ -31,7 +36,32 @@ public class CoordinatePlane {
    * I: String user typed in
    * R: ???
    */
+  public static boolean checkInput(String p) {
+    boolean status = true;
 
+    // if the point's index of 0 is equal to "("
+    // if (p.substring(0, 1).equals("(")) {
+    //   status = true;
+    // }
+    // else {
+    //   status = false;
+    // }
+
+    // is the opening parenthesis first?
+    if (p.indexOf("(") != 0) {
+      status = false;
+    } // end first if statement
+    // is there a comma and space?
+    if (p.indexOf(", ") == -1) {
+      status = false;
+    } // end second if statement
+    // is the closing parentesis last?
+    if (p.indexOf(")") != p.length() - 1) {
+      status = false;
+    } // end third if statement
+
+    return status;
+  } // end checkInput method
 
   /*
    * N: getX
